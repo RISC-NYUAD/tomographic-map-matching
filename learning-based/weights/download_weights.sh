74bc31a63d96cd48963d61040c5081d0143d7b77
H. Utku Unlu <utku@utkuunlu.com>
2024-05-03 18:34:53 +0400

    Add weights folder, update ignore rules

---
+++ learning-based/weights/download_weights.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+set -e
+
+# Some formatting utils
+show_info ()
+{
+    # Green and bold
+    FMT='\033[1;32m'
+    OFF='\033[0m'
+    echo "$FMT$1$OFF"
+}
+
+# Function to automate downloads
+download_weights ()
+{
+     mkdir -p $FOLDER
+
+    for FILE in $FILES; do
+        if [ -f "$FOLDER/$FILE" ]
+        then
+            echo "- File $FILE exists. Skipping..."
+        else
+            show_info ">> Downloading $FILE"
+            wget --show-progress --quiet $URL_BASE/$FILE -P $FOLDER
+        fi
+    done
+}
+
+# Deep Global Registration
+show_info ">> DeepGlobalRegistration"
+FOLDER=DeepGlobalRegistration
+URL_BASE=http://node2.chrischoy.org/data/projects/DGR
+FILES="ResUNetBN2C-feat32-3dmatch-v0.05.pth ResUNetBN2C-feat32-kitti-v0.3.pth"
+download_weights $FOLDER $URL_BASE $FILES
+
+# Rotation-Invariant Transformer
+show_info ">> RoITr"
+FOLDER=RoITr
+URL_BASE=https://github.com/haoyu94/RoITr/releases/download/v1.0.0
+FILES="model_3dmatch.pth"
+download_weights $FOLDER $URL_BASE $FILES
+
+# GeoTransformer
+show_info ">> GeoTransformer"
+FOLDER=GeoTransformer
+URL_BASE=https://github.com/qinzheng93/GeoTransformer/releases/download/1.0.0
+FILES="geotransformer-3dmatch.pth.tar geotransformer-kitti.pth.tar"
+download_weights $FOLDER $URL_BASE $FILES
