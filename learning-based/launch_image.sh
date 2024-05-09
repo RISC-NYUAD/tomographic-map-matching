3ace42fe7b03422b8a301ec6e94d4c9844dbe223
H. Utku Unlu <utku@utkuunlu.com>
2024-05-09 15:07:07 +0400

    Change path for the mounted directories

--- learning-based/launch_image.sh
+++ learning-based/launch_image.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-# Pass the package folder name
 PKG=$1
+
+# Ensure the necessary folders exist at least
+mkdir -p results/$PKG
+mkdir -p weights/$PKG
+
 singularity run --nv --no-home \
-    --bind $PKG/scripts:/workspace \
+    --bind $PKG/workspace:/workspace \
     --bind tmp_home:$HOME \
     --bind weights/$PKG:/weights \
     --bind results/$PKG:/results \
