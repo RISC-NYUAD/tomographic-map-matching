a869080d49caac3fa62f5dedf4ca4dd090368f80
H. Utku Unlu <utku@utkuunlu.com>
2024-05-23 11:44:47 +0400

    Folder restructuring and contanierizing Consensus

--- launch_image.sh
+++ launch_image.sh
@@ -11,6 +11,6 @@ singularity run --nv --no-home \
     --bind tmp_home:$HOME \
     --bind weights/$PKG:/weights \
     --bind results/$PKG:/results \
-    --bind ../data-config:/data-config \
+    --bind data-config:/data-config \
     --bind $DATA_DIR:/data \
     $PKG/$PKG.sif
